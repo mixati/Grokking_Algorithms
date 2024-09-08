e29d18c02455565ed40880a56b0441be9201cffe
mixati <mr.mishatit@gmail.com>
2024-09-08 20:27:01 +0300

    Сортировка выбором

--- algos/main.go
+++ algos/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
-	"algos/modules/search"
+	// "algos/modules/search"
+	"algos/modules/sort"
 	"fmt"
 )
 
 func main() {
-	var arr []int = []int{1, 3, 5, 7, 12, 15, 24}
-	var num int = 15
 
-	ans := search.BinarySearch(arr, num)
+	// БИНАРНЫЙ ПОИСК
+	// var arr []int = []int{1, 3, 5, 7, 12, 15, 24}
+	// var num int = 15
+	// ans := search.BinarySearch(arr, num)
+	// fmt.Println(ans)
+
+	// СОРТИРОВКА ВЫБОРОМ
+	var arr []int = []int{1, -5, 15, 6, 4, 3, 0}
+	ans := sort.SelectionSort(arr)
 	fmt.Println(ans)
 }
---
+++ algos/modules/sort/selection_sort.go
@@ -0,0 +1,25 @@
+package sort
+
+func findSmallest(arr []int) int {
+	smallest := arr[0]
+	smallest_id := 0
+	for i, value := range arr {
+		if value < smallest {
+			smallest = value
+			smallest_id = i
+		}
+	}
+	return smallest_id
+}
+
+func SelectionSort(arr []int) []int {
+	newArr := make([]int, len(arr))
+	arr_size := len(arr)
+	for i := 0; i < arr_size; i++ {
+		smallest := findSmallest(arr)
+		newArr[i] = arr[smallest]
+		arr[smallest] = arr[len(arr)-1]
+		arr = arr[:len(arr)-1]
+	}
+	return newArr
+}
